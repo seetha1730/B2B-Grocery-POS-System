fc905a8cd27525de09e3782fff57d62f8d5ba388
Seetha lakshmi Rajendran <seetharaj1990@gmail.com>
2023-12-07 10:23:50 +0100

    atlas changed

--- db/index.js
+++ db/index.js
@@ -9,7 +9,7 @@ const MONGO_URI =
    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/point-of-sale";
 
 mongoose
-  .connect(MONGO_URI)
+.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((x) => {
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
